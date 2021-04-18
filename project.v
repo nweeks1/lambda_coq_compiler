540bb7088dbecb412526f5957c5692d1b4e7644c
nweeks <noe.weeks@hotmail.com>
2021-04-18 12:54:54 +0200

    Finished first part of proof on parallel substitution

--- project.v
+++ project.v
@@ -1,6 +1,7 @@
 Require Import List.
 Import ListNotations.
 Require Import Arith.
+Require Import Bool.
 Inductive de_brujin : Set :=
   | Var : nat -> de_brujin
   | Abstraction : de_brujin -> de_brujin
@@ -70,6 +71,7 @@ Fixpoint increment_greater (n : nat) (u : de_brujin) : de_brujin :=
 
 Definition increment_free_vars := increment_greater 0.
 
+  
 Fixpoint subst (n : nat) (t : de_brujin) (u : de_brujin) :=
   match u with
     | Var i => 
@@ -221,6 +223,33 @@ Qed.
 Print List.Forall.
 
 
+Lemma minus_0_le : forall (n : nat) (m : nat), n-m = 0 -> n <= m.
+Proof.
+  intro.
+  induction n.
+  simpl. intros. induction m. trivial. apply le_S. trivial.
+  intro. induction m.
+  intro.
+  inversion H.
+  intro.
+  inversion H.
+  apply le_n_S. apply IHn. trivial.
+Qed.
+
+Lemma lt_add : forall (i : nat) (n : nat) (m : nat), i+n < i+m -> n < m.
+Proof.
+  intro.
+  induction i.
+  intros.
+  simpl in H. trivial.
+  intros.
+  rewrite Nat.add_succ_l in H.
+  rewrite Nat.add_succ_l in H.
+  apply lt_S_n in H.
+  apply IHi. trivial.
+Qed.
+
+
 Theorem Subst_composite : forall (i : nat) (u0 : de_brujin) (q : list de_brujin) (u : de_brujin),
   List.Forall (ClosedN i) q -> parallel_subst i (u0 :: q) u = subst i u0 (parallel_subst (S i) q u).
 Proof.
@@ -231,3 +260,128 @@ Proof.
   
   + intros.
     simpl.
+    case_eq (i <=? n).
+    intro. simpl. apply (proj1 (Nat.leb_le i n)) in H0.
+    case_eq (n <? i + S (length q)).
+    intro. simpl.
+    apply (proj1 (Nat.ltb_lt n (i + S (length q)))) in H1.
+    case_eq (n-i).
+    intro. destruct n.
+    simpl. destruct i. trivial.
+    inversion H0.
+    
+    case_eq (i <=? n).
+    intro. apply (proj1 (Nat.leb_le i n)) in H3.
+    simpl.
+    apply minus_0_le in H2.
+    apply (le_trans (S n) i n H2) in H3.
+    exfalso.
+    apply (le_not_lt n n).
+    trivial. trivial. simpl. intro.
+    clear H3.
+    apply le_plus_minus in H0.
+    rewrite H2 in H0. rewrite Nat.add_0_r in H0.
+    rewrite <- H0.
+    rewrite (proj2 (Nat.eqb_eq n n)).
+    trivial. trivial.
+    
+    intros.
+    apply le_plus_minus in H0. rewrite H2 in H0.
+    rewrite H0.
+    case_eq (i + S n0).
+    simpl. intro.
+
+    rewrite (Nat.add_succ_r i n0) in H3.
+    inversion H3.
+
+    intros.
+    rewrite (Nat.add_succ_r) in H3.
+    inversion H3. clear H3.
+    rewrite (proj2 (Nat.leb_le i (i + n0))).
+    simpl.
+    rewrite (Nat.add_succ_r) in H0.
+    rewrite <- H0.
+    rewrite (Nat.add_succ_r) in H1.
+    rewrite (proj2 (Nat.ltb_lt n (S (i + length q)))).
+
+    rewrite minus_plus.
+    trivial.
+    rewrite (ClosedN_subst i). trivial.
+    apply (proj1 (List.Forall_nth (ClosedN i) q)).
+    trivial.
+    rewrite H0 in H1.
+    apply lt_S_n in H1.
+    apply (lt_add i).
+    trivial.
+    trivial.
+    apply le_plus_l.
+
+    intro.
+    rewrite Nat.add_succ_r in H1.
+    rewrite H1.
+    rewrite andb_false_r.
+    simpl.
+    case_eq (n =? i).
+    intro.
+    apply (proj1 (Nat.eqb_eq n i)) in H2.
+    rewrite H2 in H1.
+    exfalso.
+    apply (lt_not_le i (S (i + length q))).
+
+    apply le_n_S.
+    apply le_plus_l.
+    
+    apply (proj2 (not_true_iff_false (i <? S (i + length q)))) in H1.
+    apply (proj1 (not_iff_compat (Nat.ltb_lt i (S (i + length q))))) in H1.
+    case_eq (Nat.le_gt_cases (S (i + length q)) i).
+    intros.
+    trivial.
+    intros.
+    exfalso. apply H1. trivial.
+    trivial.
+    
+    intro.
+    simpl.
+    apply (proj2 (not_true_iff_false (i <=? n))) in H0.
+    apply (proj1 (not_iff_compat (Nat.leb_le i n))) in H0.
+    case_eq n.
+    simpl. intros.
+    rewrite H1 in H0.
+    case_eq i.
+    intro.
+    rewrite H2 in H0. exfalso. apply H0. trivial.
+    intros.
+    trivial.
+    intros.
+    simpl.
+    case_eq (i <=? n0).
+    simpl. intro.
+    apply (proj1 (Nat.leb_le i n0)) in H2.
+    apply le_S in H2.
+    rewrite <- H1 in H2.
+    exfalso. apply H0. apply H2.
+    simpl.
+    intros.
+    case_eq i.
+    trivial.
+    intros.
+    case_eq (n0 =? n1).
+    intro.
+    apply (proj1 (Nat.eqb_eq n0 n1)) in H4.
+    apply eq_S in H4.
+    rewrite <- H1 in H4.
+    rewrite <- H3 in H4.
+    exfalso. apply H0.
+    rewrite H4. trivial.
+    trivial.
+
+  + intros.
+    simpl.
+    rewrite IHu.
+    trivial.
+    induction q.
+    simpl. trivial.
+    
+    simpl.
+    apply Forall_cons.
+    
